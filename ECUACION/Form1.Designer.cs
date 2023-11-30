ab76b4a10511049b60f9076b3511604497677c06
DIEGO ERICK <DIEGO ERICK@LAPTOP-1BPHSF7N>
2023-11-30 01:55:57 -0600

    Agregar archivos de proyecto.

---
+++ ECUACION/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ECUACION
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(96, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 30);
+            label1.TabIndex = 0;
+            label1.Text = "f(x)=";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.FromArgb(255, 224, 192);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(40, 132);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 124);
+            listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            listBox2.BackColor = Color.FromArgb(255, 224, 192);
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(192, 132);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(120, 124);
+            listBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(63, 262);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "FOR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 128);
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(214, 262);
+            button2.Name = "button2";
+            button2.Size = new Size(86, 23);
+            button2.TabIndex = 4;
+            button2.Text = "DO..WHILE";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 192, 128);
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(63, 292);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 5;
+            button3.Text = "WHILE";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(255, 192, 128);
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(214, 292);
+            button4.Name = "button4";
+            button4.Size = new Size(86, 23);
+            button4.TabIndex = 6;
+            button4.Text = "SALIR";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(166, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 30);
+            label2.TabIndex = 7;
+            label2.Text = "1 + x²";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(192, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(25, 30);
+            label3.TabIndex = 8;
+            label3.Text = "x";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(153, 34);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 30);
+            label4.TabIndex = 9;
+            label4.Text = "-----------";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(87, 99);
+            label5.Name = "label5";
+            label5.Size = new Size(25, 30);
+            label5.TabIndex = 10;
+            label5.Text = "x";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(229, 99);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 30);
+            label6.TabIndex = 11;
+            label6.Text = "f(x)";
+            label6.Click += label6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 192);
+            ClientSize = new Size(350, 327);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Controls.Add(label4);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "ECUACION";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
